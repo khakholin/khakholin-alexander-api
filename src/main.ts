5d1f7c013841460e1d0fad4cf58a9351237b360f
khakholin <khakholin@mail.ru>
2021-03-23 10:18:20 +0700

    upd

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('/api');
 
-  await app.listen(5555);
+  await app.listen(80);
 }
 bootstrap();
